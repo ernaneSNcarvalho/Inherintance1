47b3d379af666d16394bee560449d94b56078feb
ernanesouzacarvalho <ernanecarvalho89@gmail.com>
2021-07-21 07:13:28 -0300

    Finished Program

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Inherintance1.Entities;
+
+namespace Inherintance1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <=n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n): ");
+                char res = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                if(res == 'y' || res == 'Y')
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();            
+            Console.WriteLine("PAYMENTS: ");
+
+            foreach(Employee employee in list)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
